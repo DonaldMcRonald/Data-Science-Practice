d06f9229db54c306226fc17f733de547e1cc1bf5
DonaldMcRonald <raunak1997@utexas.edu>
2016-04-10 20:53:05 -0500

    added a Scatter Plot

--- messing_with_pyplot_dr.py
+++ messing_with_pyplot_dr.py
@@ -63,3 +63,48 @@ plt.xlabel("Decile")
 plt.ylabel("# of Students")
 plt.title("Distribution of Exam 1 Grades")
 plt.show()
+
+"""
+LINE CHARTS
+"""
+variance = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+bias_squared = [256, 128, 64, 32, 16, 8, 4, 2, 1]
+
+# Sum of variance and bias
+total_error = [x + y for x, y in zip(variance, bias_squared)]
+
+# X-axis
+xs = [i for i, _ in enumerate(variance)]
+
+# we can make multiple calls to plt.plot
+# to show multiple series on the same chart
+plt.plot(xs, variance,      'g-',   label='variance') # green solid line
+plt.plot(xs, bias_squared,  'r-.',  label='bias^2') # red dot-dashed line
+plt.plot(xs, total_error,   'b:',   label='total error') # blue dotted line
+
+# because we've assigned labels to each series # we can get a legend for free
+# loc=9 means "top center"
+plt.legend(loc=9)
+plt.xlabel("model complexity")
+plt.title("The Bias-Variance Tradeoff")
+plt.show()
+
+"""
+SCATTER PLOTS
+"""
+friends = [ 70,  65,  72,  63,  71,  64,  60,  64,  67]
+minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
+labels =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+plt.scatter(friends, minutes)
+
+# label each point
+for label, friend_count, minute_count in zip(labels, friends, minutes):
+    plt.annotate(   label,
+                    xy=(friend_count, minute_count),    # put the label with its point
+                    xytext=(5, -5),                     # but slightly offset
+                    textcoords = 'offset points')
+
+plt.title("Daily Minutes vs. Number of Friends")
+plt.xlabel("# of friends")
+plt.ylabel("daily minutes spent on the site")
+plt.show()
